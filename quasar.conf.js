0060f99cfff5bcf532137138209207365e5cfdef
Ed Tewiah <etewiah@hotmail.com>
2022-01-15 11:54:41 +0530

    Enable prefetch and use for initial webSetup

--- quasar.conf.js
+++ quasar.conf.js
@@ -16,7 +16,7 @@ module.exports = configure(function (ctx) {
     supportTS: false,
 
     // https://quasar.dev/quasar-cli/prefetch-feature
-    // preFetch: true,
+    preFetch: true,
 
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
--- src/layouts/SppLayout.vue
+++ src/layouts/SppLayout.vue
@@ -1,15 +1,10 @@
 <template>
   <q-layout view="lhh LpR ffr">
-    <!-- <SppTopAdminSection
-      :setupData="setupData"
-    ></SppTopAdminSection> -->
+    <!-- <SppTopAdminSection></SppTopAdminSection> -->
     <q-page-container class="main-layout-router-view-cont">
-      <router-view
-        :setupData="setupDataFromApi"
-        @showNotification="showNotification"
-      />
+      <router-view @showNotification="showNotification" />
     </q-page-container>
-    <div class="q-pa-md">
+    <div class="ajax-bar-container">
       <q-ajax-bar
         ref="bar"
         position="top"
@@ -17,42 +12,69 @@
         size="10px"
         :skip-hijack="false"
       />
-      <!-- <q-btn color="primary" label="Trigger" /> -->
     </div>
   </q-layout>
 </template>
 <script>
-// import coreDefaultSetup from "src/nuggets/setup/core-default.js";
 // import SppTopAdminSection from "src/packs/bvh-spp/SppTopAdminSection.vue"
 import { defineComponent, ref } from "vue";
 import { useQuasar, useMeta } from "quasar";
-// import { sharedConfig } from "boot/shared-config";
-// import { setupApi } from "boot/axios";
 export default defineComponent({
   name: "SppLayout",
   components: {
     // SppTopAdminSection,
   },
-  computed: {
-    // setupData() {
-    //   let setupData = coreDefaultSetup;
-    //   if (sharedConfig.CADDYFIED) {
-    //     setupData = this.setupDataFromApi;
-    //   }
-    //   return setupData;
-    // },
+  preFetch({
+    store,
+    currentRoute,
+    previousRoute,
+    redirect,
+    ssrContext,
+    urlPath,
+    publicPath,
+  }) {
+    let originProtocolAndHost = `${
+      ssrContext.req.protocol
+    }://${ssrContext.req.get("Host")}`;
+    // console.log(`originProtocolAndHost is ${originProtocolAndHost}`);
+    let requestDetails = {
+      originProtocolAndHost: originProtocolAndHost,
+    };
+    return store.dispatch("configStore/fetchWebConfig", requestDetails).then(
+      (data) => {
+        // console.log(data)
+      },
+      (error) => {}
+    );
   },
+  computed: {},
   setup() {
+    // const metaData = {
+    //   // // sets document title
+    //   // title: sharedConfig.defaultPageTitle,
+    //   // // optional; sets final title as "Index Page - My Website", useful for multiple level meta
+    //   // // titleTemplate: (title) => `${title} - My Website`,
+    //   // // meta tags
+    //   // meta: {
+    //   //   description: {
+    //   //     name: "description",
+    //   //     content: sharedConfig.defaultPageDesc,
+    //   //   },
+    //   //   // keywords: { name: "keywords", content: "Quasar website" },
+    //   // },
+    //   // <noscript> tags
+    //   noscript: {
+    //     default: "Please enable javascript to be able to use this site.",
+    //   },
+    // }
+    // useMeta(() => {
+    //   return metaData
+    // })
     const $q = useQuasar();
-    // const data = ref(null)
-    let setupDataFromApi = ref({
-      header_section: "",
-    });
-    function showNotification(messageToShow) {
-      $q.notify(messageToShow);
+    function showNotification(notificationMessage) {
+      $q.notify(notificationMessage);
     }
     return {
-      setupDataFromApi,
       showNotification,
       // qLang: $q.lang,
     };
