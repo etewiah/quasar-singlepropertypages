87f3683e8c36e54510a814bac2212f506e51b001
Ed Tewiah <etewiah@hotmail.com>
2022-02-17 20:59:35 +0530

    Use custom card for listingNavItem

--- src/components/nav/ListingNavItem.vue
+++ src/components/nav/ListingNavItem.vue
@@ -1,12 +1,28 @@
 <template>
   <q-item>
-    <q-btn
+    <q-card
+      v-ripple
+      clickable
+      @click="toViewListingUrl"
+      style="width: 100%"
+      :class="[`cursor-pointer q-hoverable bg-${navColor} text-white`]"
+    >
+      <q-card-section>
+        <div class="text-h6"></div>
+        <div class="text-subtitle2">{{ listingItem.title }}</div>
+      </q-card-section>
+      <!-- <q-card-actions>
+        <q-btn flat>Action 1</q-btn>
+        <q-btn flat>Action 2</q-btn>
+      </q-card-actions> -->
+    </q-card>
+    <!-- <q-btn
       class="full-width"
       :color="navColor"
       :label="listingItem.title"
-      icon="picture_as_pdf"
+      icon="list"
       :to="viewListingUrl"
-    />
+    /> -->
   </q-item>
 </template>
 <script>
@@ -20,24 +36,33 @@ export default defineComponent({
       default: () => {},
     },
   },
-  computed: {
-    viewListingUrl() {
-      let routeInputs = {
+  methods: {
+    toViewListingUrl() {
+      this.$router.push({
         name: "rSppEdit",
         params: {
           listing_slug: this.listingItem.listing_uuid,
           listings_grouping: "for-sale",
         },
-      }
-      let resolvedRouter = this.$router.resolve(routeInputs)
-      let newUrl = resolvedRouter.href
-      return newUrl
+      })
     },
+  },
+  computed: {
+    // viewListingUrl() {
+    //   let routeInputs = {
+    //     name: "rSppEdit",
+    //     params: {
+    //       listing_slug: this.listingItem.listing_uuid,
+    //       listings_grouping: "for-sale",
+    //     },
+    //   }
+    //   let resolvedRouter = this.$router.resolve(routeInputs)
+    //   let newUrl = resolvedRouter.href
+    //   return newUrl
+    // },
     isActive() {
       let activeUuid = this.$route.params.listing_slug || ""
-      if (
-        this.listingItem.listing_uuid === activeUuid
-      ) {
+      if (this.listingItem.listing_uuid === activeUuid) {
         return true
       } else {
         return false
@@ -56,7 +81,5 @@ export default defineComponent({
       // savedSppItems: [],
     }
   },
-
-  methods: {},
 })
 </script>
